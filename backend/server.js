3182d2fc732ae8699111856b0babac18f43cd37e
Alan Dlimbetov <alandlimbetov@Alans-MacBook-Pro.local>
2026-01-13 21:19:38 -0600

    Add health check

--- backend/server.js
+++ backend/server.js
@@ -31,3 +31,7 @@ mongoose.connect(process.env.MONGO_URI)
 // Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server running on", PORT));
+
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
